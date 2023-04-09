2e406692ec212910fa7e580e281993f67c74ed39
Rick Figueroa <rfig2k17@gmail.com>
2023-04-09 04:16:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet (num1, num2){
+    return Math.abs((num1-num2) * 264);
+}
+
+function calculatesFarePrice (start, destination){
+    let distance = distanceTravelledInFeet (start, destination)
+    if(distance > 2500){
+        return "cannot travel that far";
+    }
+
+    else if(distance > 2000){
+        return 25;
+    }
+    
+    else if(distance > 400){
+        return (distance - 400) * 0.02;
+    }
+
+    else{
+        return 0;
+    }
+    
+}
